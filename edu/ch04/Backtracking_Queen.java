7595e72a56db933c5ccc5885d5ebf7e9462e847e
cyccc95 <cyccc95@naver.com>
2022-10-26 14:43:27 +0900

    NQueen

--- edu/ch04/Backtracking_Queen.java
+++ edu/ch04/Backtracking_Queen.java
@@ -8,68 +8,57 @@ public class Backtracking_Queen {
         int answer = 0;
         int row, col;
         int count = 0;
-        while(count < data.length){ // 첫번째 row의 모든 col에 다 배치해볼 때까지
-            row = 0;
-            col = count;
-            while (row < data.length && col < data.length){
-                if(check(data,row,col)){
-                    data[row][col] = 1;
-                    Point p = new Point(row,col);
-                    st.push(p);
-                    row++;
-                    col = 0;
-                } else {
-                    col++;
-                }
-
-                if (col == data.length) {
-                    clearData(data, row - 1);
-                    Point pop = st.pop();
-                    row = pop.getX();
-                    col = pop.getY() + 1;
-                    if(row == 0) {
-                        break;
-                    }
-                    if(col == data.length){
-                        clearData(data, row - 1);
-                        Point pop2 = st.pop();
-                        row = pop2.getX();
-                        col = pop2.getY() + 1;
-                        if(row == 0) {
-                            break;
-                        }
-                    }
-                }
-                if (row == data.length) {
-                    print(data);
-                    for(int i = 0; i < data.length;i++) {
-                    	System.out.print("--");
-                    }
-                    System.out.println();
-                    answer++;
-                    clearData(data, row - 1);
-
-                    Point pop = st.pop();
-                    row = pop.getX();
-                    col = pop.getY() + 1;
-                    if(col == data.length){
-                        clearData(data, row - 1);
-                        Point pop2 = st.pop();
-                        row = pop2.getX();
-                        col = pop2.getY() + 1;
-                    }
-                }
-            }
-            st.clear();
-            clearData(data,0);
-            count++;
+        
+        while(count < data.length) { 					// 0행의 모든 col에 퀸을 배치해 볼 동안
+        	row = 0;									// row는 0에서 시작
+        	col = count;								// 0행에 몇개의 퀸을 배치해봤는지 체크할 count
+        	while(row < data.length) {					// 모든 row에 퀸을 배치해 볼 동안
+        		Point p = new Point(row,col);			// 현재 위치의 p 생성
+        		if(check(data,row,col)) {				// 충돌 체크
+        			data[row][col] = 1;					// 충돌 안하면 현재 위치 data에 저장
+        			st.push(p);							// 현재 위치 스택에 푸시
+        			row++; col = 0;						// 다음 행
+        		} else {								// 충돌하면
+        			col++;								// 다음 col 검사
+        		}
+        		if(col >= data[0].length) {				// 각 행에서 만족하는 col이 없으면
+        			p = st.pop();						// 스택에서 이전 위치 팝
+        			row = p.getX(); col = p.getY();		// 이전 위치를 현 위치로 저장
+        			data[row][col] = 0;					// 이전 위치 데이터 삭제
+        			col++;								// 이전 위치에서 col + 1해서 다음 위치부터 검사
+        			if(row == 0) {						// 반복하다가 팝한 데이터가 0행이면 count 올려서 다음 케이스 검사
+        				count++;
+        				break;
+        			}
+        		}	
+        		if(row == data.length) {				// 모든 row에 배치해서 정답이 나오면
+        			print(data); answer++;	
+        			for(int i = 0; i < st.size();i++){  // 정답 출력하고 경우의 수 +
+        				System.out.print("--");								
+        			}
+        			System.out.println();
+        			p = st.pop();						// 이전 위치 팝
+        			row = p.getX(); col = p.getY();		// 현재 위치로 저장
+        			data[row][col] = 0;					// 데이터에서 이전 위치 삭제
+        			col++;								// col + 1 해서 다음 위치부터 검사
+        		}
+        	}
         }
         System.out.println(answer);
     }
     public static boolean check(int[][] data, int row, int col){
         int i,j;
+     // 같은 row에 이미 퀸이 있는지 체크
+        for(i = 0; i < data.length; i++){
+            if(data[row][i] == 1){
+                return false;
+            }
+        }
         // 같은 col에 이미 퀸이 있는지 체크
-        for(i = 0; i < row; i++){
+        for(i = 0; i < data.length; i++){
+        	if(col >= data.length) {
+        		return false;
+        	}
             if(data[i][col] == 1){
                 return false;
             }
