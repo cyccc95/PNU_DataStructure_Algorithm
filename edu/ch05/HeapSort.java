645886dfc2fcdf743b4cc96f48dbcaef6676afaa
cyccc95 <cyccc95@naver.com>
2022-11-04 09:58:39 +0900

    heapSort

--- edu/ch05/HeapSort.java
+++ edu/ch05/HeapSort.java
@@ -1,60 +1,142 @@
 package ch05;
 
-public class HeapSort {
-	
-	static void makeHeap(int[] a, int count) {
-		for(int i = 0; i < count; i++) {
-			int j = i;
-			while(j >= 0) {
-				if(a[j] > a[(j - 1) / 2]) {
-					swap(a, j, (j - 1) / 2);
-				}
-				j--;
-			}
-		}
-		
-	}
-	
-	static void useHeap(int[] a, int count) {
-		while(count > 0) {
-			swap(a, 0, count - 1);
-			count--;
-			makeHeap(a, count);
-		}
-	}
-	
-	static void swap(int[] a, int idx1, int idx2) {
-	     int temp = a[idx1];  
-	     a[idx1] = a[idx2];  
-	     a[idx2] = temp;
-	 }
-	public static void main(String[] args) {
-		 final int count = 10;
-	     int[] a = new int[count];
-//		 int[] a = {5,3,6,10,8,7,13,4};
-//		 int count = a.length;
-	     for (int i = 0; i < a.length; i++) {
-	         a[i] = (int)(Math.random() * 30);
-	     }
-	     System.out.println("---정렬전---");
-	     for (int i = 0; i < count; i++) {
-	    	 System.out.print(" " + a[i]);
-	     }
-	     
-	     makeHeap(a, count);
-	     System.out.println();
-	     System.out.println("---makeHeap---");
-	     for (int i = 0; i < count; i++) {
-	    	 System.out.print(" " + a[i]);
-	     }
-	     
-	     useHeap(a, count);    // 배열 x를 힙정렬
-	     System.out.println();
-	     System.out.println("---useHeap---");
-	     for (int i = 0; i < count; i++) {
-	    	 System.out.print(" " + a[i]);
-	     }
-	}
-	
+import java.util.Scanner;
 
+class Element implements Comparable<Element>{
+    private int key;
+
+    public Element(int key) {
+        this.key = key;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    @Override
+    public int compareTo(Element o) {
+        if (this.key < o.key) return -1;
+        else if (this.key > o.key) return 1;
+        else return 0;
+    }
+}
+
+interface MaxPQ {
+    public void insert(Element key);
+    public void deleteMax();
+}
+
+class MaxHeap implements MaxPQ {
+    private Element[] heap;
+    private int maxSize;	// Maximum allowable size of MaxHeap
+    private int n;			// current size of MaxHeap
+
+    public MaxHeap(int size) {
+        maxSize = size;
+        n = 0;
+        heap = new Element[maxSize + 1]; // Don't want to use heap[0]
+    }
+
+    @Override
+    public void insert(Element key) {
+        if (n == maxSize) {
+            heapFull(); return;
+        } else {
+            n++;
+        }
+        for (int i = n; i >= 1; i /= 2) {
+            heap[i] = key;
+            if (i == 1) break; // at root
+            if (key.compareTo(heap[i / 2]) < 1){
+                break;
+            }
+            // move from parent to i
+            heap[i] = heap[i / 2];
+            heap[i / 2] = key;
+        }
+    }
+
+    @Override
+    public void deleteMax() {
+        int i, j; // i parent, j child
+        if (n == 0) {
+            heapEmpty(); return;
+        }
+        if (n == 1) {	// 마지막 1개만 남았으면 포인터를 -해서 0으로 주고 리턴
+        	n--;
+        	return;
+        }
+        Element temp = heap[1];
+        heap[1] = heap[n];
+        heap[n] = temp; n--;
+
+        for (i = 1, j = 2; j <= n; ) {
+            if (j < n && heap[j].compareTo(heap[j + 1]) < 0) j++;
+            // j points to the larger child
+            if (j <= n && heap[i].compareTo(heap[j]) < 0) {
+                temp = heap[j];
+                heap[j] = heap[i];
+                heap[i] = temp;
+            }
+            i = j; j *= 2;
+        }
+    }
+    void display() {
+        for(int i = 1; i <= n; i++){
+        	System.out.print(heap[i].getKey() + " ");
+        }
+    }
+    void heapSort() {	// deleteMax를 모두 수행한 뒤에 오름차순 정렬된 배열을 출력
+    	for(int i = 1; i <= maxSize; i++) {
+    		System.out.print(heap[i].getKey() + " ");
+    	}
+    }
+    void heapEmpty() {
+        System.out.println("Heap Empty");
+    }
+    void heapFull() {
+        System.out.println("Heap Full");
+    }
+}
+
+public class HeapSort {
+
+    static Scanner sc = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        MaxHeap heap = new MaxHeap(10);
+
+        while(true){
+            System.out.println();
+            System.out.print("(1) insert (2) delete (3) display (4) heapsort (5) exit (입력) : ");
+            int select = sc.nextInt();
+
+            if(select == 5) break;
+            switch (select){
+                case 1:
+                    System.out.print("insert : ");
+                    int insert = sc.nextInt();
+                    Element key = new Element(insert);
+                    heap.insert(key);
+                    break;
+                case 2:
+                    heap.deleteMax();
+                    System.out.println();
+                    break;
+                case 3:
+                    heap.display();
+                    System.out.println();
+                    break;
+                case 4:
+                	heap.heapSort();
+                	System.out.println();
+                	break;
+            }
+        }
+
+    }
 }
