fa7b232daf20ea85ac1e99e5a5f8751c7fa49109
cyccc95 <cyccc95@naver.com>
2022-11-14 12:14:36 +0900

    ChainHash

--- edu/ch08/ChainHashTest.java
+++ edu/ch08/ChainHashTest.java
@@ -1,73 +1,48 @@
 package ch08;
-//hash node가 student 객체일 때를 구현하는 과제
-//체인법에 의한 해시
+
 import java.util.Scanner;
 
-class Student {
-	static final int NO   = 1;        // 번호를 읽어 들일까요?
-	static final int NAME = 2;        // 이름을 읽어 들일까요?
-
-	private Integer no;                      // 회원번호(키값)
-	private String  name;                    // 이름
-
-	//--- 키값 ---//
-	public Integer keyCode() {
-		return this.no;
-	}
-
-	//--- 문자열 표현을 반환 ---//
-	public String toString() {
-		return "(" + this.no + ") " + this.name + " ";
-	}
-	public Student(Integer snum, String sname) {
-		this.no = snum;
-		this.name = sname;
-	}
-	public Student(Student s) {
-		this.no = s.no;
-		this.name = s.name;
-	}
-	public Student() {
-		this.no = null;
-		this.name = null;
-	}
-	//--- 데이터를 읽어 들임 ---//
-
-}
 class ChainHash {
 	//--- 해시를 구성하는 노드 ---//
 	class Node {
-		private Student data;                 // 키값
+		private Student data;     // 키값
 		private Node next;        // 뒤쪽 포인터(뒤쪽 노드에 대한 참조)
 
-		//--- 생성자(constructor) ---//
+		public Node() {
+			this.data  = null;
+			this.next = null;
+		}
+
 		public Node(Student s) {
 			this.data = new Student(s);
 			this.next = null;
 		}
-		Node(Student s, Node p) {
+		
+		public Node(Student s, Node p) {
 			this.data = s;
 			this.next = p;
 		}
-		Node() {
-			this.data  = null;
-			this.next = null;
-		}
+		
 		//--- 키값을 반환 ---//
-		Integer getKey() {
-			return this.data.keyCode();
+		public Integer getKey() {
+			return this.data.getNo();
+		}
+		
+		//--- 값을 반환 ---//
+		public String getValue() {
+			return this.data.getName();
 		}
 
 		//--- 키의 해시값을 반환 ---//
 		public int hashCode() {
 			int hash = 11;
-			hash = 31* hash * getKey();
+			hash = 31* hash * getKey().intValue();
 			hash = hash * hash;
-			return hash;
+			return hash % 13;
 		}
 	}
 
-	private int size;              // 해시 테이블의 크기
+	private int size;            // 해시 테이블의 크기
 	private Node[] table;        // 해시 테이블
 
 	//--- 생성자(constructor) ---//
@@ -81,41 +56,73 @@ class ChainHash {
 	}
 
 	//--- 해시값을 구함 ---//
-	public int hashValue(Object key) {
+	public int hashValue(Integer key) {
 		int hash = 11;
-		hash = 31 * hash * (int)key;
+		hash = 31 * hash * key.intValue();
 		hash = hash * hash;
-		return hash;
+		return hash % 13;
 	}
 
 	//--- 키값이 key인 요소를 검색(데이터를 반환) ---//
-	public int search(int key) {
-		int hash = hashValue(key);            // 검색할 데이터의 해시값
-		Node p = table[hash];            // 선택 노드
-
+	public String search(Integer key) {
+		int hash = hashValue(key);	// 검색할 데이터의 해시값
+		Node p = table[hash];       // 선택 노드
+		int cnt = 1;
 		while (p != null) {
 			if (p.getKey() == key)
-				return p.getKey();                // 검색 성공
-			p = p.next;                             // 다음 노드를 선택
+				return "(" + p.getKey() + ") " + p.getValue() + " : " + hash + "테이블의 " + cnt + "에 있습니다.";	// 검색 성공
+			p = p.next;             // 다음 노드를 선택
+			cnt++;
 		}
-		return 0;                                // 검색 실패
+		return "그 데이터는 없습니다.";                   // 검색 실패
 	}
 
 	//--- 키값이 key인 데이터를 data의 요소로 추가 ---//
-	public int add(Student st) {
-		int hash = hashValue(st.keyCode()) % 9;            // 추가할 데이터의 해시값
-		Node p = table[hash];            // 선택 노드
-		//구현 필요함
+	public void add(Student st) {
+		int hash = hashValue(st.getNo());	// 추가할 데이터의 해시값
+		Node p = table[hash];            	// 선택 노드
+		if (p == null) {
+			Node input = new Node(st);
+			table[hash] = input;
+			return;
+		}
+		while(p != null) {
+			if(p.getKey() == st.getNo()) {
+				System.out.println("그 데이터는 이미 존재합니다.");
+				return;
+			}
+			p = p.next;
+		}
+		Node input = new Node(st,table[hash]);
+		table[hash] = input;
 	}
-
+	
 	//--- 키값이 key인 요소를 삭제 ---//
-	public int remove(int key) {
-		int hash = hashValue(key);            // 삭제할 데이터의 해시값
+	public void remove(Integer key) {
+		int hash = hashValue(key);       // 삭제할 데이터의 해시값
 		Node p = table[hash];            // 선택 노드
 		Node pp = null;                  // 바로 앞의 선택 노드
-		//구현실습 
-		return 1;                             // 찾는 키값이 없음	
+		
+		while (p != null) {
+			if (p.getKey() == key) {
+				break;
+			}
+			pp = p;
+			p = p.next;             
+		}
+		if (p == null) {
+			System.out.println("그 데이터는 없습니다.");
+			return;
+		} 
+		if(pp == null) {
+			table[hash] = p.next;
+		} else {
+			pp.next = p.next;
+		}
+		System.out.println("(" + key + ") " + p.getValue() + "를 삭제");
+		
 	}
+	
 
 	//--- 해시 테이블을 덤프(dump) ---//
 	public void dump() {
@@ -123,69 +130,55 @@ class ChainHash {
 			Node p = table[i];
 			System.out.printf("%02d  ", i);
 			while (p != null) {
-				System.out.printf("→ %s ", p.getKey());
+				System.out.printf("→ (%d) %s ",p.getKey(),p.getValue());
 				p = p.next;
-	       }
-	       System.out.println();
+			}
+			System.out.println();
 		}
 	}
 }
 
 
 public class ChainHashTest {
-	static Scanner stdIn = new Scanner(System.in);
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		ChainHash hash = new ChainHash(11);
-		Student data;
+		ChainHash hash = new ChainHash(13);
 		int select = 0;
-		final int count = 3;
 		while (select != 6) {
-			System.out.println(1);
-			System.out.println(
-					"SimpleChainHash. Select 1:Add, 2. Delete, 3:Search, 4. PrintDump, 5. Quit =>");
-
-			select = stdIn.nextInt();
+			System.out.print("SimpleChainHash:: Select 1:Add, 2:Delete, 3:Search, 4:PrintDump, 5:Quit => ");
+			select = sc.nextInt();
 			switch (select) {
-			case 1:
-				Student[] input = new Student[count];
-				Integer sno = 0;
-				String sname = null;
-				for (int ix = 0; ix < count; ix++) {
-
-	                 System.out.println("입력 데이터(sno, sname):: ");
-
-	                 System.out.print("번호: ");
-	                 sno = stdIn.nextInt();
-
-	                 System.out.print("이름: ");
-	                 sname = stdIn.next();
-	                 System.out.print("sno =  " + sno);
-	                 input[ix] = new Student(sno, sname);
-	                  hash.add(input[ix]);
-				
-					System.out.print(" " + input[ix]);
+				case 1: // add
+					Student input = new Student();
+	                System.out.print("번호: ");
+	                input.setNo(sc.nextInt());
+	                System.out.print("이름: ");
+	                input.setName(sc.next());
+	                hash.add(input);
+	                System.out.println(input.toString());
+					break;
+				case 2: // Delete
+					System.out.print("Key to delete : ");
+					hash.remove(sc.nextInt());
+					break;
+				case 3:
+					System.out.print("Key to search : ");
+					int val = sc.nextInt();
+					System.out.println(hash.search(val));
+					break;
+				case 4:
+					hash.dump();
+					break;
+				case 5:
+					System.out.println("Quit");
+					break;
+				default:
+					System.out.println("WRONG INPUT  ");
+					System.out.println("Re-Enter");
+					break;
 				}
-				break;
-			case 2:
-				// Delete
-				break;
-			case 3:
-				System.out.println("Search Value:: ");
-				int val = stdIn.nextInt();
-				hash.search(val);
-				System.out.println();
-				break;
-			case 4:
-				hash.dump();
-				break;
-			case 5:
-				System.out.println("Quit");
-				break;
-
-			default:
-				System.out.println("WRONG INPUT  ");
-				System.out.println("Re-Enter");
+			if (select == 5) {
 				break;
 			}
 		}
